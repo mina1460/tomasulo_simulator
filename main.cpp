f9f445b8b9daa4375f12be485d64db05b1869de2
Sherif Gabr <50206867+shgabr@users.noreply.github.com>
2020-12-09 21:57:32 +0200

    Half Done 2

--- main.cpp
+++ main.cpp
@@ -14,17 +14,28 @@ After Register Renaming
     Execute:    When both operands are ready (RaW) & branch outcome known
     Write:      After execution, NO condition       
 http://people.ee.duke.edu/~sorin/ece252/lectures/4.2-tomasulo.pdf
+
+
+To Do:
+    - JALR does not reissue instructions after jumping backwards
+    - LW rs1 dependancy stalls execution
+    - SW rs2 dependancy stalls execution
+    - Try BEQ, RET, NEG, DIV   
+    - Add another stopping condition
+    - First instruction takes 1 extra cycle in execution
+    - User enters starting program's address 
+
 */
 
 
 int main(){
 
     //cout << "Enter cycles for : LW SW BEQ JALR/RET ADD/NEG/ADDI DIV:";
-    int lc(1), sc(1), bc(1), jc(1), ac(3), dc(1);
+    int lc(2), sc(2), bc(1), jc(2), ac(3), dc(1);
     // cin >> lc >>  sc >> bc >> jc >> ac >> dc;
     
     //cout << "Enter reservation stations for : LW SW BEQ JALR/RET ADD/NEG/ADDI DIV:";
-    int lr(1), sr(1), br(1), jr(1), ar(3), dr(1);
+    int lr(2), sr(2), br(1), jr(1), ar(3), dr(1);
    // cin >> lr >> sr >> br >> jr >> ar >> dr;
 
     Tomasulu tomasulu(lr, sr, br, jr, ar, dr, "./instructions.txt", lc, sc, bc, jc, ac, dc);
--- old.asm
+++ old.asm
@@ -10,3 +10,12 @@ RET
 ADDI      x1, x0, 32
 ADDI      x5, x0, 10
 ADD       x2, x5, x1
+
+ADDI x4, x0, 3
+ADDI x2, x4, 20
+ADDI x2, x2, 5
+ADDI x6, x0, 6
+JALR x4
+ADDI x7, x0, 4
+ADD x0, x0, x4
+ADD x0, x0, x2
